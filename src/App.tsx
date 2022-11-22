25ae53213a0b4b08ba978f1f2e9a2a7cd5bb9323
portableclass <av.yashutkin@gmail.com>
2022-11-22 17:58:48 +0300

    setup env

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './assets/styles/scss/app.scss'
+
+export default function App() {
+    return (
+        <div className='app-wrapper'>
+            <h1>keyman24</h1>
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'static',
+};
+
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist';
+}
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        title: 'keyman24',
+        template: `${PATHS.src}/index.html`,
+        favicon: `${PATHS.src}/assets/img/favicon.ico`,
+    }),
+    new MiniCssExtractPlugin({
+        filename: `${PATHS.assets}/css/[name].[contenthash].css`,
+    }),
+];
+
+if (process.env.SERVE) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+}
+
+module.exports = {
+    mode,
+    target,
+    plugins,
+    entry: {
+        main: `${PATHS.src}/index.tsx`,
+    },
+    output: {
+        path: PATHS.dist,
+        filename: `${PATHS.assets}/js/[name].[contenthash].js`,
+        assetModuleFilename: `${PATHS.assets}/[hash][ext][query]`,
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        hot: true,
+        open: {
+            app: {
+                name: 'Google Chrome',
+            },
+        },
+        port: 3001,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: mode === 'production' ? 'asset' : 'asset/resource',
+                generator: {
+                    filename: `${PATHS.assets}/img/[hash][ext][query]`,
+                },
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: `${PATHS.assets}/fonts/[hash][ext][query]`,
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
